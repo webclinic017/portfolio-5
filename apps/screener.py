03f86afe96ac9cb1a1021bc40d191ecead49b31d
nishant <cool250@outlook.com>
2020-05-24 21:27:44 -0700

    Removed screener_view

--- apps/screener.py
+++ apps/screener.py
@@ -2,11 +2,11 @@ import pandas as pd
 
 import dash
 import dash_bootstrap_components as dbc
+import dash_html_components as html
 from dash.dependencies import Input, Output, State
 from dash_table import DataTable
 
 from app import app
-from .views import screener_view
 from utils.constants import screener_list
 from utils.constants import style_cell, style_header, style_data_conditional
 
@@ -18,7 +18,153 @@ from service.option_strategies import (
     long_call
 )
 
-layout = screener_view.layout
+TOP_COLUMN = dbc.Jumbotron(
+    [
+        html.H5(children="Income Finder"),
+        html.Hr(className="my-2"),
+        dbc.Row(
+            [
+                dbc.Col(
+                    dbc.FormGroup(
+                        [
+                            dbc.Label("Choose one"),
+                            dbc.RadioItems(
+                                options=[
+                                    {"label": "SECURED PUT", "value": "PUT"},
+                                    {"label": "COVERED CALL", "value": "CALL"},
+                                ],
+                                value="PUT",
+                                id="contract_type",
+                                inline=True,
+                            ),
+                        ]
+                    ),
+                    width=6,
+                ),
+                dbc.Col(
+                    dbc.FormGroup(
+                        [
+                            dbc.Label("WatchList"),
+                            dbc.Select(
+                                options=[
+                                    {"label": i, "value": i} for i in screener_list
+                                ],
+                                value="",
+                                id="ticker_list",
+                            ),
+                        ]
+                    ),
+                    width=3,
+                ),
+                dbc.Col(
+                    dbc.FormGroup(
+                        [
+                            dbc.Label("Ticker", html_for="example-email-grid"),
+                            dbc.Input(type="text", id="ticker", placeholder="",),
+                        ]
+                    ),
+                    width=3,
+                ),
+            ],
+        ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    dbc.FormGroup(
+                        [
+                            dbc.Label("Min Exp Days", html_for="example-email-grid"),
+                            dbc.Input(
+                                type="text", id="min_expiration_days", placeholder="15",
+                            ),
+                        ]
+                    ),
+                    width=2,
+                ),
+                dbc.Col(
+                    dbc.FormGroup(
+                        [
+                            dbc.Label("Max Exp Days", html_for="example-password-grid"),
+                            dbc.Input(
+                                type="text", id="max_expiration_days", placeholder="45",
+                            ),
+                        ]
+                    ),
+                    width=2,
+                ),
+                dbc.Col(
+                    dbc.FormGroup(
+                        [
+                            dbc.Label("Min Delta", html_for="example-email-grid"),
+                            dbc.Input(type="text", id="min_delta", placeholder="0.25",),
+                        ]
+                    ),
+                    width=2,
+                ),
+                dbc.Col(
+                    dbc.FormGroup(
+                        [
+                            dbc.Label("Max Delta", html_for="example-email-grid"),
+                            dbc.Input(type="text", id="max_delta", placeholder="0.35",),
+                        ]
+                    ),
+                    width=2,
+                ),
+                dbc.Col(
+                    dbc.FormGroup(
+                        [
+                            dbc.Label("Premium %", html_for="premium"),
+                            dbc.Input(type="text", id="premium", placeholder="2",),
+                        ]
+                    ),
+                    width=2,
+                ),
+                dbc.Col(
+                    dbc.FormGroup(
+                        [
+                            dbc.Label("Discount/Gain %", html_for="moneyness"),
+                            dbc.Input(type="text", id="moneyness", placeholder="5",),
+                        ]
+                    ),
+                    width=2,
+                ),
+            ]
+        ),
+        dbc.Row(
+            dbc.Col(
+                dbc.Button(
+                    "Search",
+                    color="primary",
+                    className="float-right",
+                    id="screener-btn",
+                ),
+            ),
+        ),
+    ],
+    className="container-fluid",
+)
+
+SEARCH_RESULT = [
+    dbc.Col(
+        [
+            html.Div(
+                dbc.Alert(
+                    id="screener-message",
+                    is_open=False,
+                    duration=2000,
+                    color="danger",
+                ),
+            ),
+            html.Div(dbc.Spinner(html.Div(id="screener-output"))),
+        ]       
+    ),
+]
+
+layout = html.Div(
+    [
+        dbc.Row(TOP_COLUMN, className="justify-content-center"),
+        dbc.Row(SEARCH_RESULT, className="justify-content-center"),
+    ],
+)
 
 @app.callback(
     [Output('screener-output', 'children'),
