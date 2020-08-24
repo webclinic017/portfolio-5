80a5b29cac9a38b583e0b3f683e980aa513b063c
nishant <cool250@outlook.com>
2020-08-24 15:22:03 -0700

    config.ini

--- utils/store.py
+++ utils/store.py
@@ -1,21 +1,33 @@
-import redis
 import json
+import redis
+from utils.config import Config
+from utils.exceptions import HaltCallbackException
 
-class Store():
 
-    HOST = 'localhost'
-    PORT = 6379
-    PWD = ""
-
+class Store:
     def __init__(self):
-        self.client = redis.StrictRedis(host=self.HOST, port=self.PORT, password=self.PWD, decode_responses=True)
+        config = Config()
+        self.client = redis.StrictRedis(
+            host=config.get("REDIS", "HOST"),
+            port=config.get("REDIS", "PORT"),
+            password=config.get("REDIS", "PWD"),
+            decode_responses=True,
+        )
 
     def set_dict(self, key, val):
         # Convert Dict to JSON string
         json_val = json.dumps(val)
-        self.client.set(key, json_val)
-    
-    def get_dict (self, key):
-        json_string =  self.client.get(key)
+        try:
+            self.client.set(key, json_val)
+        except redis.exceptions.ConnectionError as err:
+            raise HaltCallbackException("Unable to connect", err)
+
+    def get_dict(self, key):
+        try:
+            json_string = self.client.get(key)
+        except redis.exceptions.ConnectionError as err:
+            raise HaltCallbackException("Unable to connect", err)
+
         # Convert JSON string to Dict
         return json.loads(json_string)
+
