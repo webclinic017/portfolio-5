5544b9ae2139e034c6ceadcfafa1062012957bb6
nishant <cool250@outlook.com>
2020-08-24 15:29:59 -0700

    changes suppress callback code

--- app.py
+++ app.py
@@ -14,9 +14,15 @@ VALID_USERNAME_PASSWORD_PAIRS = [
 external_stylesheets=[dbc.themes.COSMO]
 app = dash.Dash(__name__)
 
+# Since we're adding callbacks to elements that don't exist in the app.layout,
+# Dash will raise an exception to warn us that we might be
+# doing something wrong.
+# In this case, we're adding the elements through a callback, so we can ignore
+# the exception.
 app = dash.Dash(
     __name__,
-    external_stylesheets= external_stylesheets
+    external_stylesheets= external_stylesheets,
+    suppress_callback_exceptions=True
 )
 
 app.title = 'Options Tracker'
@@ -26,4 +32,3 @@ auth = dash_auth.BasicAuth(
     VALID_USERNAME_PASSWORD_PAIRS
 )
 server = app.server
-app.config.suppress_callback_exceptions = True
